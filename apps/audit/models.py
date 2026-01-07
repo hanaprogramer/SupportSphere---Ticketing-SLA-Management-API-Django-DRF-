667d34cb107a056c4404a84ab2a4001c327f1869
hana <hanahemati9@gmail.com>
2026-01-07 16:07:04 +0330

    Add audit model and admin setup

--- apps/audit/models.py
+++ apps/audit/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from apps.accounts.models import *
+class AuditLog(models.Model):
+    ACTION_TYPE_CHOICES = [
+        ("CREATE","create"),
+        ("UPDATE","update"),
+        ("DELETE","delete")
+        ]
+    
+    OBJECT_TYPE = [
+        ("TICKET","ticket"),
+        ("REPLY","reply"),
+        ("PLANREQUEST","planrequest"),
+        ("ETC","etc")
+        ]
+    
+    actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="audit_logs")
+    action_type = models.CharField(max_length=10, choices=ACTION_TYPE_CHOICES)
+    object_type = models.CharField(max_length=15, choices=OBJECT_TYPE)
+    object_id = models.PositiveIntegerField()   # The ID is something that has changed
+    before_data = models.JSONField(null=True, blank=True)
+    # What was the situation before the change?
+    # for example : {"status": "closed", "priority": "high"}
+    after_data = models.JSONField(null=True, blank=True)
+    # What was the situation after the change?
+    # for example : {"status": "closed", "priority": "high"}
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.actor.email} {self.action_type} {self.object_type}({self.object_id})"
+
